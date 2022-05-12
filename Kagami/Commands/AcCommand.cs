a21dd221092185219a6a51cf4d50570e00017278
舰队的偶像-岛风酱! <frg2089@outlook.com>
2022-05-12 17:34:00 +0800

    添加AcFun业务逻辑

---
+++ Kagami/Commands/AcCommand.cs
@@ -0,0 +1,37 @@
+﻿using Konata.Core.Message;
+
+namespace Kagami.Commands;
+
+/// <summary>
+/// 从Acfun通过ac号获取视频信息
+/// </summary>
+public sealed class AcCommand : IKagamiCommand
+{
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public string Command { get; } = "av";
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public string Description { get; } = "从AcFun通过ac号获取视频信息";
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public (Type, string)[] Arguments { get; } = new[] {
+        (typeof(uint), "ac号")
+    };
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public Task<MessageBuilder> InvokeAsync(string[] args)
+        => Services.AcFun.GetVideoInfoFrom($"ac{args[0]}");
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public CommandType CommandType => CommandType.Prefix;
+}
---
+++ Kagami/Services/AcFun.cs
@@ -0,0 +1,58 @@
+using System.Diagnostics;
+using Kagami.Utils;
+using Konata.Core.Message;
+
+namespace Kagami.Services;
+
+/// <summary>
+/// 和A站相关的业务逻辑代码
+/// </summary>
+public static class AcFun
+{
+    /// <summary>
+    /// 获取A站的视频基本信息
+    /// </summary>
+    /// <param name="code">aid/bvid</param>
+    /// <returns></returns>
+    public static async Task<MessageBuilder> GetVideoInfoFrom(string code)
+    {
+        var uri = $"https://www.acfun.cn/v/{code}";
+        Debug.WriteLine($"[{nameof(AcFun)}]::({nameof(GetVideoInfoFrom)}): Get From: \"{uri}\"");
+
+        // UrlDownload the page
+        var html = await uri.DownloadString();
+        // Get meta data
+        string titleMeta = string.Empty;
+        string imageMeta = string.Empty;
+        string descriptionMeta = string.Empty;
+
+        const string flag = "window.pageInfo = window.videoInfo = ";
+        StringReader sr = new(html);
+        while (sr.Peek() >= 0)
+        {
+            var line = await sr.ReadLineAsync();
+            if (line?.Contains(flag) ?? false)
+            {
+                var raw_json = line.Replace(flag, string.Empty).Trim().TrimEnd(';');
+                var json = System.Text.Json.JsonDocument.Parse(raw_json);
+                if (json.RootElement.TryGetProperty("title", out var element))
+                    titleMeta = element.GetString() ?? string.Empty;
+                if (json.RootElement.TryGetProperty("description", out element))
+                    descriptionMeta = element.GetString() ?? string.Empty;
+                if (json.RootElement.TryGetProperty("coverUrl", out element))
+                    imageMeta = element.GetString() ?? string.Empty;
+                break;
+            }
+        }
+
+        // UrlDownload the image
+        var image = await imageMeta.DownloadBytes();
+
+        // Build message
+        return new MessageBuilder($"{titleMeta}")
+            .TextLine(descriptionMeta)
+            .TextLine(uri)
+            .TextLine()
+            .Image(image);
+    }
+}
