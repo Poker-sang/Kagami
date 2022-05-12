1bb7b84272037dcb774243ed4a9960d1d1d69f2d
舰队的偶像-岛风酱! <frg2089@outlook.com>
2022-05-12 21:19:17 +0800

    添加复读机命令

---
+++ Kagami/Commands/RepeatCommand.cs
@@ -0,0 +1,40 @@
+﻿using Kagami.Utils;
+using Konata.Core.Exceptions.Model;
+using Konata.Core.Interfaces.Api;
+using Konata.Core.Message;
+using Konata.Core.Message.Model;
+
+namespace Kagami.Commands;
+
+/// <summary>
+/// <inheritdoc/>
+/// </summary>
+public sealed class RepeatCommand : IKagamiCommand
+{
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public string Command { get; } = "repeat";
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public string Description { get; } = "复读机";
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public (Type, string)[] Arguments { get; } = new[] {
+        (typeof(MessageChain), "内容")
+    };
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public async Task<MessageBuilder> InvokeAsync(Konata.Core.Bot? bot, Konata.Core.Events.Model.GroupMessageEvent? group, object[] args)
+    {
+        if (args[0] is not MessageChain message)
+            return new(await StringResources.ArgumentErrorMessage.RandomGetAsync());
+        return Services.Kernel.Repeat(message);
+    }
+}
