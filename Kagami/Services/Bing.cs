5f21ef842a63321d34786b69b75538fbab09ee28
舰队的偶像-岛风酱! <frg2089@outlook.com>
2022-05-12 19:07:27 +0800

    Bing 业务逻辑

---
+++ Kagami/Services/Bing.cs
@@ -0,0 +1,27 @@
+using System.Text.Json;
+using Kagami.Utils;
+using Konata.Core.Message;
+using Konata.Core.Message.Model;
+
+namespace Kagami.Services;
+
+public static class Bing
+{
+    private const string BASE_URI = "https://cn.bing.com/";
+    private const string IMAGE = "HPImageArchive.aspx?n=1&format=js";
+
+    public static async Task<MessageBuilder> PictureAsync()
+    {
+        JsonDocument json = JsonDocument.Parse(await (BASE_URI + IMAGE).DownloadString());
+        var obj = json.RootElement.GetProperty("images")[0];
+        string uri = obj.GetProperty("url").GetString()!.TrimStart('/');
+        var data = await (BASE_URI + uri).UrlDownloadBytes();
+        return new MessageBuilder()
+            .Image(data)
+            .TextLine()
+            .TextLine(obj.GetProperty("title").GetString()!)
+            .TextLine()
+            .TextLine(obj.GetProperty("copyright").GetString()!)
+            .TextLine(BASE_URI + uri);
+    }
+}
