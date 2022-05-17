42d4d36e99da4261e064cd4a64b2601b09a23586
舰队的偶像-岛风酱! <frg2089@outlook.com>
2022-05-17 17:55:24 +0800

    添加构建信息

---
+++ Kagami.Test/BuildStampTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kagami.Test;
+
+[TestClass]
+public class BuildStampTest
+{
+    [TestMethod("BuildStamp验证")]
+    public void GenerateBuildStampTest()
+    {
+        var list = typeof(Entry)
+            .Assembly
+            .GetCustomAttributes<AssemblyMetadataAttribute>()
+            .ToDictionary(
+                i => i.Key,
+                i => i.Value);
+
+        Assert.IsTrue(list.Count is not 0);
+        Console.WriteLine(list["Branch"]);
+        Console.WriteLine(list["Revision"]);
+        Console.WriteLine(list["BuildTime"]);
+    }
+}
