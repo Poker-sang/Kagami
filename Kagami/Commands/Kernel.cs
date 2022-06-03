7085fe40c532362535d06f700dbabd5d550fa4b9
Poker <poker_sang@outlook.com>
2022-06-03 15:03:51 +0800

    分离逻辑

--- Kagami/Commands/Kernel.cs
+++ Kagami/Commands/Kernel.cs
@@ -4,32 +4,36 @@ using Konata.Core;
 using Konata.Core.Events.Model;
 using Konata.Core.Interfaces.Api;
 using Konata.Core.Message;
+using Konata.Core.Message.Model;
 using System.ComponentModel;
 
 namespace Kagami.Commands;
 
 public static class Kernel
 {
+    // private static readonly WeakReference<MessageBuilder?> sPing = new(null); // sPing.Get(() => new("Pong!"))
+
     [Cmdlet(nameof(Ping)), Description("看看我是否还在线")]
-    public static MessageBuilder Ping() => Services.Kernel.Ping;
+    public static MessageBuilder Ping() => new("Pong!");
 
     [Cmdlet(nameof(Greeting)), Description("自我介绍")]
-    public static MessageBuilder Greeting() => Services.Kernel.Greeting;
+    public static MessageBuilder Greeting() => new("你好!!! 我是Poker Kagami!");
 
     [Cmdlet(nameof(Status)), Description("内核信息")]
-    public static MessageBuilder Status() => Services.Kernel.Status();
+    public static MessageBuilder Status() => new(Services.Kernel.Status());
 
     [Cmdlet(nameof(Repeat)), Description("复读一条消息")]
-    public static MessageBuilder Repeat(GroupMessageEvent group) => Services.Kernel.Repeat(group.Chain);
+    public static MessageBuilder Repeat(GroupMessageEvent group)
+        => new MessageBuilder(((TextChain)group.Chain[0]).Content[nameof(Repeat).Length..].Trim()).Add(group.Chain[1..]);
 
     [Cmdlet(nameof(Roll)), Description("帮我选一个")]
     public static MessageBuilder Roll([Description("一些选项")] string[] items)
-        => Services.Kernel.Roll(items[1..]);
+        => new(Services.Kernel.Roll(items[1..]));
 
     [Cmdlet(nameof(Member)), Description("获取成员信息")]
     public static async ValueTask<MessageBuilder> Member(Bot bot, GroupMessageEvent group,
         [Description("成员")] At at)
-        => await bot.GetGroupMemberInfo(group.GroupUin, at.Uin, true) is { } memberInfo
+        => new(await bot.GetGroupMemberInfo(group.GroupUin, at.Uin, true) is { } memberInfo
             ? Services.Kernel.Member(memberInfo)
-            : new("没有找到这个人x");
+            : "没有找到这个人x");
 }
--- Kagami/Services/Kernel.cs
+++ Kagami/Services/Kernel.cs
@@ -1,7 +1,5 @@
 using Kagami.Core;
 using Konata.Core.Common;
-using Konata.Core.Message;
-using Konata.Core.Message.Model;
 using System.Diagnostics;
 
 namespace Kagami.Services;
@@ -11,78 +9,52 @@ namespace Kagami.Services;
 /// </summary>
 public static class Kernel
 {
-    private static readonly WeakReference<MessageBuilder?> sPing = new(null);
-    private static readonly WeakReference<MessageBuilder?> sGreeting = new(null);
-
-    /// <summary>
-    /// Ping
-    /// </summary>
-    public static MessageBuilder Ping => sPing.Get(() => new("Pong!"));
-
-    /// <summary>
-    /// 自我介绍
-    /// </summary>
-    public static MessageBuilder Greeting => sGreeting.Get(() => new("你好!!! 我是Poker Kagami!"));
-
-    /// <summary>
-    /// 复读机
-    /// </summary>
-    /// <param name="chain">消息</param>
-    /// <returns></returns>
-    public static MessageBuilder Repeat(MessageChain chain)
-        => new MessageBuilder(((TextChain)chain[0]).Content[nameof(Repeat).Length..].Trim()).Add(chain[1..]);
-
     /// <summary>
     /// 获取成员信息
     /// </summary>
     /// <returns></returns>
     /// <exception cref="ArgumentOutOfRangeException"></exception>
-    public static MessageBuilder Member(BotMember memberInfo)
-        => new MessageBuilder($"[{memberInfo.NickName}]")
-            .TextLine($"群名片：{memberInfo.Name}")
-            .TextLine($"加入时间：{(memberInfo.JoinTime is 0
-                ? "未知" : DateTimeOffset.FromUnixTimeSeconds(memberInfo.JoinTime).LocalDateTime)}")
-            .TextLine($"类别：{memberInfo.Role switch
-            {
-                RoleType.Member => "成员",
-                RoleType.Admin => "管理员",
-                RoleType.Owner => "群主",
-                _ => throw new ArgumentOutOfRangeException("memberInfo.Role", "未知的类别")
-            }}")
-            .TextLine($"等级：{memberInfo.Level}")
-            .TextLine($"头衔：{memberInfo.SpecialTitle}");
+    public static string Member(BotMember memberInfo)
+        => @$"[{memberInfo.NickName}]
+群名片：{memberInfo.Name}
+加入时间：{(memberInfo.JoinTime is 0 ? "未知" : DateTimeOffset.FromUnixTimeSeconds(memberInfo.JoinTime).LocalDateTime)}
+类别：{memberInfo.Role switch
+        {
+            RoleType.Member => "成员",
+            RoleType.Admin => "管理员",
+            RoleType.Owner => "群主",
+            _ => throw new ArgumentOutOfRangeException(nameof(memberInfo.Role), "未知的类别")
+        }}
+等级：{memberInfo.Level}
+头衔：{memberInfo.SpecialTitle}";
 
     /// <summary>
     /// 状态
     /// </summary>
     /// <returns></returns>
-    public static MessageBuilder Status()
-    => new MessageBuilder("[Konata.Core] 内核信息")
-        // Core descriptions
-        .TextLine($"[分支:{KonataBuildStamp.Branch}]")
-        .TextLine($"[提交:{KonataBuildStamp.CommitHash[..12]}]")
-        .TextLine($"[版本:{KonataBuildStamp.Version}]")
-        .TextLine($"[{KonataBuildStamp.BuildTime}]")
-        .TextLine("Konata Project © 2022")
-        .TextLine()
-        .TextLine("[Poker Kagami] 构建信息")
-        .TextLine($"[分支:{KagamiBuildStamp.Branch}]")
-        .TextLine($"[提交:{KagamiBuildStamp.Revision}]")
-        .TextLine($"[版本:{KagamiBuildStamp.Version}]")
-        .TextLine($"[{KagamiBuildStamp.BuildTime}]")
-        .TextLine("Poker Kagami Project © 2022 frg2089 & Poker, All Rights Reserved.")
-        .TextLine()
-        // System status
-        .TextLine($"处理了 {BotResponse.MessageCounter.Values.Aggregate(0, (a, b) => a + b)} 条消息")
-        .TextLine($"GC内存 {GC.GetTotalAllocatedBytes().Bytes2MiB(2)} MiB ")
-        .Text($"({Math.Round((double)GC.GetTotalAllocatedBytes() / GC.GetTotalMemory(false) * 100, 2)}%)")
-        .TextLine($"总内存 {Process.GetCurrentProcess().WorkingSet64.Bytes2MiB(2)} MiB");
+    public static string Status()
+    => @$"[Konata.Core] 内核信息
+[分支:{KonataBuildStamp.Branch}]
+[提交:{KonataBuildStamp.CommitHash[..12]}]
+[版本:{KonataBuildStamp.Version}]
+[{KonataBuildStamp.BuildTime}]
+Konata Project © 2022
+    
+[Poker Kagami] 构建信息
+[分支:{KagamiBuildStamp.Branch}]
+[提交:{KagamiBuildStamp.Revision}]
+[版本:{KagamiBuildStamp.Version}]
+[{KagamiBuildStamp.BuildTime}]
+Poker Kagami Project © 2022 frg2089 & Poker, All Rights Reserved.
 
-    public static MessageBuilder Roll(string[] items)
-        => new(items.Length < 2
+处理了 {BotResponse.MessageCounter.Values.Aggregate(0, (a, b) => a + b)} 条消息
+GC内存 {GC.GetTotalAllocatedBytes().Bytes2MiB(2)}MiB ({Math.Round((double)GC.GetTotalAllocatedBytes() / GC.GetTotalMemory(false) * 100, 2)}%)
+总内存 {Process.GetCurrentProcess().WorkingSet64.Bytes2MiB(2)}MiB";
+
+    public static string Roll(string[] items)
+        => items.Length < 2
             ? "没有选项让我怎么选，笨！"
-            : string.Format(StringResources.RollMessage.RandomGet(), items.RandomGet()));
-
+            : string.Format(StringResources.RollMessage.RandomGet(), items.RandomGet());
 
     public static double Bytes2MiB(this long bytes, int round) => Math.Round(bytes / 1048576.0, round);
 }
