d74cd95f95a7bdb13154091559514b0ab304b133
Poker <poker_sang@outlook.com>
2022-06-18 13:34:34 +0800

    Saucenao

---
+++ Kagami/Commands/SauceNao.cs
@@ -0,0 +1,13 @@
+﻿using Kagami.ArgTypes;
+using Kagami.Attributes;
+using Konata.Core.Message;
+using System.ComponentModel;
+
+namespace Kagami.Commands;
+
+public static class SauceNao
+{
+    [Cmdlet(nameof(Sauce)), Description("从SauceNao搜索图源")]
+    public static async ValueTask<MessageBuilder> Sauce(Image image)
+        => new(await Services.SauceNao.Search(image.Url));
+}
---
+++ Kagami/Services/SauceNao.cs
@@ -0,0 +1,31 @@
+﻿using Kagami.ArgTypes;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace Kagami.Services;
+
+public static class SauceNao
+{
+    private const string key = "924b8942229c980c731c98e13b89f3a755568e4e";
+    private const string api = "https://saucenao.com/search.php";
+
+    public static async Task<string> Search(string imageUrl)
+    {
+        var picParams = new StringContent(new JsonObject
+        {
+            ["url"] = imageUrl,
+            ["db"] = 999,
+            ["api_key"] = key,
+            ["output_type"] = 2,
+            ["numres"] = 3
+        }.ToJsonString());
+
+        var client = HttpClientExtensions.Client.InitializeHeader();
+        using var res = await client.PostAsync(api, picParams);
+        if (!res.IsSuccessStatusCode)
+            return new("获取小说nid错误...");
+        var nidData = (await JsonDocument.ParseAsync(await res.Content.ReadAsStreamAsync()))
+            .RootElement;
+        return "s";
+    }
+}
